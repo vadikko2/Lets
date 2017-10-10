95e4f99e22787987162b0e54a871425a30eccf34
Vadim Kozirevsky <vadikko2@mail.ru>
2017-10-10 22:05:26 +0300

    Fix EventActivity Add event on Click registration button

--- app/src/main/java/com/example/root/myapplication/EventActivity.java
+++ app/src/main/java/com/example/root/myapplication/EventActivity.java
@@ -1,7 +1,9 @@
 package com.example.root.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class EventActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class EventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+    }
+
+    public void registration(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
