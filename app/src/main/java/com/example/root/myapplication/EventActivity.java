a6f43536159262e740f8c8710275f1324fba8cd2
Vadim Kozirevsky <vadikko2@mail.ru>
2017-10-12 12:58:16 +0300

    Fix design create activity

--- app/src/main/java/com/example/root/myapplication/EventActivity.java
+++ app/src/main/java/com/example/root/myapplication/EventActivity.java
@@ -1,16 +1,19 @@
 package com.example.root.myapplication;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class EventActivity extends AppCompatActivity {
-
+public class EventActivity extends AppCompatActivity{
+    Button share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
     }
 
     public void registration(){
--- app/src/main/java/com/example/root/myapplication/ItemEventsFragment.java
+++ app/src/main/java/com/example/root/myapplication/ItemEventsFragment.java
@@ -2,22 +2,14 @@ package com.example.root.myapplication;
 
 import android.content.Intent;
 import android.support.v4.app.ListFragment;
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -26,7 +18,8 @@ import java.util.HashMap;
  * Created by root on 09.10.17.
  */
 
-public class ItemEventsFragment extends ListFragment{// определяем массив типа String
+public class ItemEventsFragment extends ListFragment {// определяем массив типа String
+    ImageButton flag;
     String[] creators={"Ander Herera","Diego Costa","Juan Mata","David De Gea","Thibaut Courtouis","Van Persie","Oscar"};
     int[] images={R.drawable.ic_action_event_empty,R.drawable.ic_action_event_empty,R.drawable.ic_action_event_empty,R.drawable.ic_action_event_empty,R.drawable.ic_action_event_empty,R.drawable.ic_action_event_empty,R.drawable.ic_action_event_empty};
     int[] imagesFlag={R.drawable.ic_action_registrate_default,R.drawable.ic_action_registrate_default,R.drawable.ic_action_registrate_default,R.drawable.ic_action_registrate,R.drawable.ic_action_registrate_default,R.drawable.ic_action_registrate_default,R.drawable.ic_action_registrate_default};
@@ -55,13 +48,13 @@ public class ItemEventsFragment extends ListFragment{// определяем м�
         //ADAPTER
         adapter=new SimpleAdapter(getActivity(), data, R.layout.fragment_events, from, to);
         setListAdapter(adapter);
+
         return super.onCreateView(inflater, container, savedInstanceState);
     }
     @Override
     public void onStart() {
         // TODO Auto-generated method stub
         super.onStart();
-
         getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> av, View v, int pos,
@@ -69,10 +62,9 @@ public class ItemEventsFragment extends ListFragment{// определяем м�
                 // TODO Auto-generated method stub
                 Intent intent = new Intent(getActivity(), EventActivity.class);
                 startActivity(intent);
-                Toast.makeText(getActivity(), data.get(pos).get("Creator"), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), data.get(pos).get("Player"), Toast.LENGTH_SHORT).show();
             }
         });
     }
-
 }
 
